669f69511e3f85ea99788ccec4ff6c0a453e535e
Suv807 <suvrajitdatta1999@gmail.com>
2022-08-05 22:40:28 +0530

    oops1

--- Prime Number/main.py
+++ Prime Number/main.py
@@ -1,6 +1,26 @@
+class Person():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
 
-for i in range(1,10):
-    if (i%2==0):
-        print("not prime")
-    else:
-        print("prime")
+    def Display(self):
+        print(f"the name is {self.name}")
+        print(f"the name is {self.age}")
+
+class Student():
+    def __init__(self,name,age,section):
+        self.section=section
+        self.name = name
+        self.age = age
+    def Display_student(self):
+        print(f"the name is {self.name}")
+        print(f"the name is {self.age}")
+
+        print(f"the name is {self.section}")
+
+student=Person("Ravi kumar",28)
+student.Display()
+print("*******************************")
+
+my=Student("L",9,"M")
+my.Display_student()
